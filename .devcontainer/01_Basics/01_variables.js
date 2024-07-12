2b69f270ef9bf0c30d5aff3fa726768a05957d1b
Hemant Dhakane <148945792+unitedThunder@users.noreply.github.com>
2024-07-12 06:48:54 +0000

    variables

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,9 @@
+var accountId= 123
+const accountName= "Hemant"
+let accountPassword="hem@123"
+
+console.log(accountId);
+console.log(accountName);
+console.log(accountPassword);
+
+console.table([accountId,accountName,accountPassword])
