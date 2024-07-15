3112e0c7c8f51f4e7a0fcff4117e7d7b6e978eaa
Hemant Dhakane <148945792+unitedThunder@users.noreply.github.com>
2024-07-15 07:33:08 +0000

    Data_Conversion

---
+++ .devcontainer/01_Basics/02_Data_Conversion.js
@@ -0,0 +1,19 @@
+let num = 10
+console.log(typeof num)
+
+let numToString = String(num)
+console.log(numToString)
+console.log(typeof numToString)
+
+let nam = "hemant"
+console.log(typeof nam)
+
+let stringToNum = Number(nam)
+console.log(stringToNum)
+console.log(typeof stringToNum)
+
+let stringToBoolean = Boolean(nam)
+console.log(stringToBoolean)
+console.log(typeof stringToBoolean)
+
+
