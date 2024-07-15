9d91a61f84ac8e04b7d305e6a78acdd65bbe06cf
Hemant Dhakane <148945792+unitedThunder@users.noreply.github.com>
2024-07-15 07:33:27 +0000

    Data_Types

---
+++ .devcontainer/01_Basics/dataTypes.js
@@ -0,0 +1,27 @@
+// Primative data type
+
+// Data typesin javascript : String, Number, BigIntger, Boolean, Symbol, null, undefined
+
+
+// reference (Non primative) : array, object, function
+
+// Array
+const arr = [1,2,3,4];
+
+// Object
+let obj = {
+    name : "hemant",
+    age : 27
+}
+
+// function
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof arr);
+
+console.log(typeof obj);
+
+console.log(typeof myFunction);
